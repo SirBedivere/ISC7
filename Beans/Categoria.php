3da4729d3258c74b229f0bedc896eaae1b4391b7
Yucaterco <kiki_espadas@hotmail.com>
2015-10-08 20:13:17 -0500

    Bean - Servicio Categoria
    Se agregó Bean y Servicio de Categoria

---
+++ Beans/Categoria.php
@@ -0,0 +1,16 @@
+<?php
+	public class Categoria{
+	
+	var $id_categoria;
+	var $status;
+	var $fecha_creacion;
+	var $ropa;
+	var $calzado;
+	var $electronica;
+	var $perfumeria;
+	var $jugueteria;
+	var $utensilios_hogar;
+	var $otros;
+	
+	}
+?>
