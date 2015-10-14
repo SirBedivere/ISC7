3fa8de37396df8da10e70b465592b015dd252ae1
samuel070192 <samuel_dzul@outlook.com>
2015-10-13 19:46:43 -0500

    listo
    samuel

--- Service/UsuarioServicio.php
+++ Service/UsuarioServicio.php
@@ -1,8 +1,10 @@
 <?php 
+include("../Conexion/Conexion.php");
+include("../Beans/Usuario.php");
 
 class Usuario {
 
-	public function insertar($objeto) {
+	public function insert($objeto) {
 		$conexion=new conexion();
 		$sql="Insert into usuario (id, apellido, nombre, email, password, imagen) Values ('".$usuario->id."','".$usuario->apellido."','".$usuario->nombre."','".$usuario->email."','".$usuario->password."','".$usuario->imagen."');";
 		$conexion->execute($sql);
@@ -12,8 +14,7 @@ class Usuario {
 	public function actualizar($objeto) {
 		$conexion=new conexion();
 		$sql="Update into usuario (id, apellido, nombre, email, password, imagen) Values ('".$usuario->id."','".$usuario->apellido."','".$usuario->nombre."','".$usuario->email."','".$usuario->password."','".$usuario->imagen."');";
-		mysqli_query($conexion->obtener(), $sql);
-		return mysqli_fetch_object($consulta);
+		return $conexion->ejecutar($sql);
 
 	}
 
@@ -21,8 +22,7 @@ class Usuario {
 		$conexion = new conexion();
 		$sql="* DELET * FROM usuario WHERE idUsuario="+$id+";";
 		$sql = $dao->delete($id);
-		mysqli_query($conexion->obtener(), $sql);
-		return mysqli_fetch_object($consulta);
+		return $conexion->ejecutar($sql);
 
 	}
 
@@ -30,7 +30,7 @@ class Usuario {
 		$conexion = new conexion();
 		$sql="* SELECT * FROM usuario WHERE idUsuario="+$id+";";
 		$consulta = mysqli_query($conexion->obtener(), $sql);
-		return mysqli_fetch_object($consulta);
+		return $conexion->ejecutar($sql);
 	}
 
 	public function obtener_todos() {
@@ -43,11 +43,11 @@ class Usuario {
 			$resultados[] = $row;
 		}
 
-		public function obtener_por_nombre($nombre) {
+	public function obtener_por_nombre($nombre) {
 		$conexion = new conexion();
 		$sql="* SELECT * FROM usuario WHERE idUsuario="+$nombre+";";
 		$consulta = mysqli_query($conexion->obtener(), $sql);
-		return mysqli_fetch_object($consulta);
+		return $conexion->ejecutar($sql);
 	}
 
 		return $resultados;	
