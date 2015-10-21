c34efac1e6bbe6886c8ed4433db0bb0f32784b8e
samuel070192 <samuel_dzul@outlook.com>
2015-10-20 20:08:20 -0500

    modal

---
+++ Design/Modal.php
@@ -0,0 +1,59 @@
+<html>
+	<head>
+		<script src="jquery.min.js">
+		</script>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		
+		<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="main.css">
+
+	</head>
+	<body>
+		<!-- Button trigger modal -->
+		<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+		  Iniciar Sesión
+		</button>
+
+		<!-- Modal -->
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		      </div>
+
+		      <div class="row">
+			      <div class="col-md-3 col-xs-3">
+			      </div>
+			      <div class="col-md-6 col-xs-6">
+			      		<label class="col-md-4 col-xs-4"> Usuario</label> <input type="text" />
+			      </div>
+			      <div class="col-md-3 col-xs-3">
+			      </div>
+			   </div>
+			</br>
+
+		      	<div class="row">
+			      <div class="col-md-3 col-xs-3">
+			      </div>
+			      <div class="col-md-6 col-xs-6">
+			      		<label class="col-md-4 col-xs-4"> Password</label> <input type="password" /> 
+			      </div>
+			      <div class="col-md-3 col-xs-3">
+			      </div>
+			   	</div>
+
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-default">Registrarse</button>
+		        <button type="button" class="btn btn-primary">Iniciar</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+	</body>
+</html>
