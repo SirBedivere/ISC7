b8ac7955456b3bde80ad5dd8f93e0335153e8fa9
Yucaterco <kiki_espadas@hotmail.com>
2015-10-20 20:18:46 -0500

    Se corrigió
    Listo

--- Design/Navegador.php
+++ Design/Navegador.php
@@ -21,6 +21,12 @@
 				
 				<div class="col-md-1 col-xs-1 col-sm-1">
 				Logo
+				
+					<div class="row">
+					<div class="col-md-1 col-xs-1 col-sm-1">
+					Logo
+					</div>
+					</div>
 				</div>
 				
 				<div class="col-md-5 col-xs-5 col-sm-5">
@@ -40,15 +46,12 @@
 				
 				<div class="row">
 				
-						<div class="col-md-2 col-xs-2 col-sm-2">
+						<div class="col-md-3 col-xs-3 col-sm-3">
 						
 						</div>
 						
-						<div class="col-md-1 col-xs-1 col-sm-1">
-						Logo
-						</div>
 						
-						<div class="col-md-5 col-xs-5 col-sm-5">
+						<div class="col-md-5 col-xs-5 col-sm-5"">
 						<button class="btn btn-primary glyphicon glyphicon-th-large btn" type="button" >
 									Inicio
 									</button>
@@ -71,7 +74,6 @@
 							<div class="form-group">
 							<input type="text" class="form-control" placeholder="Search">
 							</div>
-							<button type="submit" class="btn btn-default">Submit</button>
 							</form>
 						</div>
 						
