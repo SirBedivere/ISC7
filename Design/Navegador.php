0b8006124c9c7324a9b6c489ae988bd868598471
p0ncho14 <alfonso.falcon@gmail.com>
2015-10-20 20:06:25 -0500

    te toca gay
    (viejo)

---
+++ Design/Navegador.php
@@ -0,0 +1,87 @@
+<html>
+	<title>
+	
+	</title>
+		
+		<head>
+		
+			<script src="../Css/jquery.min.js">
+			</script>
+			<link rel="stylesheet" href="../Css/bootstrap/css/bootstrap.min.css"/>
+			<link rel="stylesheet" href="../Css/bootstrap/css/bootstrap-theme.min.css"/>
+			<script src="../Css/bootstrap/js/bootstrap.min.js">
+			</script>
+			<link rel="stylesheet" type="text/css" href="../Css/main.css"/>		
+		</head>
+		
+			<body>
+				<div class="row">
+				<div class="col-md-2 col-xs-2 col-sm-2">
+				</div>
+				
+				<div class="col-md-1 col-xs-1 col-sm-1">
+				Logo
+				</div>
+				
+				<div class="col-md-5 col-xs-5 col-sm-5">
+				Nada----------------------------------
+				</div>
+				
+				<div class="col-md-2 col-xs-2 col-sm-2">
+				<button class="btn btn-primary glyphicon glyphicon-user btn" type="button">
+				Iniciar Sesion
+				</button>
+				</div>
+				
+				<div class="col-md-2 col-xs-2 col-sm-2">
+				</div>
+				
+				</div>
+				
+				<div class="row">
+				
+						<div class="col-md-2 col-xs-2 col-sm-2">
+						
+						</div>
+						
+						<div class="col-md-1 col-xs-1 col-sm-1">
+						Logo
+						</div>
+						
+						<div class="col-md-5 col-xs-5 col-sm-5">
+						<button class="btn btn-primary glyphicon glyphicon-th-large btn" type="button" >
+									Inicio
+									</button>
+									
+									<button class="btn btn-primary glyphicon glyphicon-envelope btn" type="button">
+									Contacto
+									</button>
+									
+									<button class="btn btn-primary glyphicon glyphicon-globe btn" type="button">
+									Sucursales
+									</button>
+									
+									<button class="btn btn-primary glyphicon glyphicon-user btn" type="button" >
+									Categorias
+									</button>			
+						</div>
+						
+						<div class="col-md-2 col-xs-2 col-sm-2 form-group">	
+							<form class="navbar-form navbar-left" role="search">
+							<div class="form-group">
+							<input type="text" class="form-control" placeholder="Search">
+							</div>
+							<button type="submit" class="btn btn-default">Submit</button>
+							</form>
+						</div>
+						
+						<div class="col-md-2 col-xs-2 col-sm-2">
+						</div>
+				
+				</div>
+		
+		
+		
+			</body>
+		
+</html>
