81033f00d3830b52718a05942430d0c664a31d80
Daniel Cervantes <daniel.cervantes.avila@gmail.com>
2015-10-22 19:31:11 -0500

    Actualización del encabezado
    actualización del encabezado

--- Design/Navegador.php
+++ Design/Navegador.php
@@ -17,75 +17,53 @@
 		
 			<body>
 				<div class="row">
-				<div class="col-md-2 col-xs-2 col-sm-2">
-				</div>
-				
-				<div class="col-md-1 col-xs-1 col-sm-1">
-				
-				<img src="../Css/logo.jpg" class="img-responsive">
-					<div class="row">
 					<div class="col-md-1 col-xs-1 col-sm-1">
-					
 					</div>
+				
+					<div class="col-md-2 col-xs-2 col-sm-2">
+						<img src="../Css/logo.jpg" class="img-responsive">
+					</div>
+					<div class="col-md-8 col-xs-8 col-sm-8" >
+						<div class="row">
+							<div class="col-md-8 col-xs-8 col-sm-8">
+								Nada----------------------------------
+							</div>
+							<div class="col-md-4 col-xs-4 col-sm-4">
+								<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+									Iniciar Sesión
+								</button>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-8 col-xs-8 col-sm-8">
+								<button class="btn btn-primary glyphicon glyphicon-th-large btn" type="button" >
+								Inicio
+								</button>
+									
+								<button class="btn btn-primary glyphicon glyphicon-envelope btn" type="button">
+								Contacto
+								</button>
+									
+								<button class="btn btn-primary glyphicon glyphicon-globe btn" type="button">
+								Sucursales
+								</button>
+									
+								<button class="btn btn-primary glyphicon glyphicon-user btn" type="button" >
+								Categorias
+								</button>
+							</div>
+							<div class="col-md-4 col-xs-4 col-sm-4">
+								<div class="input-group">
+									<input type="text" class="form-control" placeholder="Buscar">
+									<span class="input-group-btn">
+									<button class="btn btn-default" type="button">Ir</button>
+							  		</span>
+							  	</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-1 col-xs-1 col-sm-1">
 					</div>
 				</div>
-				
-				<div class="col-md-5 col-xs-5 col-sm-5">
-				Nada----------------------------------
-				</div>
-				
-					<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-					Iniciar Sesión
-					</button>
-				
-				<div class="col-md-2 col-xs-2 col-sm-2">
-				</div>
-				
-				</div>
-				
-				<div class="row">
-				
-						<div class="col-md-3 col-xs-3 col-sm-3">
-						
-						</div>
-						
-						
-						<div class="col-md-5 col-xs-5 col-sm-5"">
-						<button class="btn btn-primary glyphicon glyphicon-th-large btn" type="button" >
-									Inicio
-									</button>
-									
-									<button class="btn btn-primary glyphicon glyphicon-envelope btn" type="button">
-									Contacto
-									</button>
-									
-									<button class="btn btn-primary glyphicon glyphicon-globe btn" type="button">
-									Sucursales
-									</button>
-									
-									<button class="btn btn-primary glyphicon glyphicon-user btn" type="button" >
-									Categorias
-									</button>			
-						</div>
-						
-							<div class="col-md-2 col-xs-2 col-sm-2 form-group">								
-							<div class="input-group">
-							  <input type="text" class="form-control" placeholder="Buscar">
-							  <span class="input-group-btn">
-							  <button class="btn btn-default" type="button">Ir</button>
-							  </span>
-							  </div>
-						    </div>
-						
-						<div class="col-md-2 col-xs-2 col-sm-2">
-						
-						</div>
-				
-				</div>
-		
-		
 			<?php include("Modal.php"); ?>
 		
-			</body>
-		
-</html>
