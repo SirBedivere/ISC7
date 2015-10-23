9b2ad6cd1858f73e07798ef6936c1cc0adbc4950
p0ncho14 <alfonso.falcon@gmail.com>
2015-10-22 19:45:07 -0500

    actualizo "Navegador.php"

--- Design/Navegador.php
+++ Design/Navegador.php
@@ -4,7 +4,7 @@
 	</title>
 		
 		<head>
-		
+			<meta charset=utf-8>
 			<script src="../Css/jquery.min.js">
 			</script>
 			<link rel="stylesheet" href="../Css/bootstrap/css/bootstrap.min.css"/>
@@ -17,19 +17,19 @@
 		
 			<body>
 				<div class="row">
-					<div class="col-md-1 col-xs-1 col-sm-1">
+					<div class="col-md-2 col-xs-2 col-sm-2">
 					</div>
 				
-					<div class="col-md-2 col-xs-2 col-sm-2">
-						<img src="../Css/logo.jpg" class="img-responsive">
+					<div class="col-md-1 col-xs-1 col-sm-1">
+						<img src="../Css/logo.jpg" width="100" height="100" class="img-responsive">
 					</div>
-					<div class="col-md-8 col-xs-8 col-sm-8" >
+					<div class="col-md-7 col-xs-7 col-sm-7" >
 						<div class="row">
 							<div class="col-md-8 col-xs-8 col-sm-8">
-								Nada----------------------------------
+						
 							</div>
 							<div class="col-md-4 col-xs-4 col-sm-4">
-								<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+								<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
 									Iniciar Sesión
 								</button>
 							</div>
@@ -62,7 +62,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-md-1 col-xs-1 col-sm-1">
+					<div class="col-md-2 col-xs-2 col-sm-2">
 					</div>
 				</div>
 			<?php include("Modal.php"); ?>
