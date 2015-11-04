e26322a174c9f161fe0c1199e2154f14799b6a18
Yucaterco <kiki_espadas@hotmail.com>
2015-11-03 20:13:22 -0600

    Agregado Funcion
    Eliminar Producto

---
+++ Funciones/EliminarProducto.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once ('../Conexion/Conexion.php');
+require_once ('../Beans/Producto.php');
+require_once ('../Service/ProductoServicio.php');
+
+
+
+	$CodigoBarras=$_POST['CodigoBarras'];
+	$Nombre=$_POST['Nombre'];
+	$Descripcion=$_POST['Descripcion'];
+	
+	$ProductoServicio = new ProductoServicio();
+	
+
+		
+	$Producto = new Producto();
+	$Producto->CodigoBarras=$CodigoBarras;
+	$Producto->Nombre=$Nombre;
+	$Producto->Descripcion=$Descripcion;
+	
+	$ProductoServicio->delete ($Producto);
+	
+	header('Location: ../Views/altaProducto.php');
+	die();
+?>
