f8964d4eba9fc31caea609e999b5e9b49e80ffec
CarlosArceo <master_carlos_1@hotmail.com>
2015-11-17 20:38:27 -0600

    Pepe
    pepe

--- views/catalogoProductos.php
+++ views/catalogoProductos.php
@@ -5,35 +5,32 @@ require_once "../Service/ProductoServicio.php";
 
 // productos
 $productoServicio = new ProductoServicio();
-$productos = $productoServicio->findAll();
+$categoria = isset($_GET['categoria']) ? $_GET['categoria']:"";
+if($categoria!=""){
+	$productos = $productoServicio->findByCategoria($categoria);
+}else{
+	$productos = $productoServicio->findAll();
+}
 
 ?>
-<html>
-<head>
-
-	<meta charset="utf-8" />	
-
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js"></script>
-
-</head>
-<body>
-
-<div class="row">
-	<div class="col-md-12">
-<div class="btn-group  pull-right">
-	<a href="index.php?view=newproduct" class="btn btn-default">Agregar Producto</a>
-<div class="btn-group pull-right">
-  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-    <i class="fa fa-download"></i> Descargar <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" role="menu">
-    <li><a href="">(.docx)</a></li>
-  </ul>
+<div class="Encabezado">
+	<?php include "../Design/navegador.php";?>
 </div>
-</div>
-		<h1>Lista de Productos</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="btn-group  pull-right">
+					<a href="index.php?view=newproduct" class="btn btn-default">Agregar Producto</a>
+				<div class="btn-group pull-right">
+					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+						<i class="fa fa-download"></i> Descargar <span class="caret"></span>
+					</button>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="">(.docx)</a></li>
+						</ul>
+				</div>
+				</div>
+<h1>Lista de Productos</h1>
 		<div class="clearfix"></div>
 
 
@@ -42,31 +39,44 @@ $productos = $productoServicio->findAll();
 <div class="clearfix"></div>
 <br><table class="table table-bordered table-hover">
 	<thead>
+		<th>Id</th>
 		<th>Codigo</th>
 		<th>Imagen</th>
 		<th>Nombre</th>
+		<th>Descripcion</th>
 		<th>Precio Entrada</th>
 		<th>Precio Salida</th>
 		<th>Categoria</th>
 		<th>Minima</th>
 		<th>Activo</th>
 		<th></th>
+		
 	</thead>
 	Producto
-	<?php foreach($producto as $productos){ ?>
+	<?php foreach($productos as $producto ){ ?>
 	<tr>
-		<td><?php echo $producto->id_producto; ?></td>
+		<td><?php echo $producto->Id; ?></td>
 		<td><?php echo $producto->CodigoBarras; ?></td>
+		<td><?php echo $producto->Imagen; ?></td>
 		<td><?php echo $producto->Nombre; ?></td>
 		<td><?php echo $producto->Descripcion; ?></td>
 		<td><?php echo $producto->PrecioEntrada; ?></td>
-		<td></td>
-		<td><td>
-		<td></td>
+		<td><?php echo $producto->PrecioSalida; ?></td>
+		<td><?php echo $producto->Categoria; ?></td>
+		<td><?php echo $producto->InventarioMinimo; ?></td>
+		<td><?php echo $producto->Activo; ?></td>
+		
+		<td style="width:70px;">
+		<a href="index.php?view=editproduct&id=1" class="btn btn-xs btn-warning"><i class="glyphicon glyphicon-pencil"></i></a>
+		<a href="index.php?view=delproduct&id=1" class="btn btn-xs btn-danger"><i class="glyphicon glyphicon-trash"></i></a>
+		</td>
 
-	</tr>
+		</tr>
 	<?php } ?>
 </table>
+<div class="btn-group pull-right">
+<a href='index.php?view=products&limit=10&page=1' class='btn btn-default btn-sm'>1</a> </div>
+
 <div class="btn-group pull-right">
 
 </div>
@@ -87,5 +97,7 @@ $productos = $productoServicio->findAll();
 <br><br><br><br><br><br><br><br><br><br>
 	</div>
 </div>
-</body>
-</html>
+</div>
+<div class="Pie">
+	<?php include "../Design/Pie.php";?>
+</div>
