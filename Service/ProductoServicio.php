8f74b42f00ea389e97048fc5d735b4f9e07ebfea
CarlosArceo <master_carlos_1@hotmail.com>
2015-11-17 20:39:03 -0600

    listo
    listo

--- Service/ProductoServicio.php
+++ Service/ProductoServicio.php
@@ -8,7 +8,7 @@ class  ProductoServicio{
 	 */
 	public function insert($Producto) {
 		$conexion=new conexion();
-		$sql="insert into producto (CodigoBarras, Nombre, Descripcion, PrecioEntrada) Values ('".$Producto->CodigoBarras."','".$Producto->Nombre."','".$Producto->Descripcion."','".$Producto->PrecioEntrada."');";
+		$sql="insert into producto (CodigoBarras, Imagen, Nombre, Descripcion, PrecioEntrada, PrecioSalida, CategoriaId, InventarioMin, EsActivo) Values ('".$Producto->CodigoBarras."',".$Producto->Imagen.",'".$Producto->Nombre."','".$Producto->Descripcion."','".$Producto->PrecioEntrada.",".$Producto->PrecioSalida.",".$Producto->CategoriaId.",".$Producto->InventarioMin.",".$Producto->EsActivo."');";
 		$resultado=$conexion->ejecutar($sql);
 		return $resultado;
 	}
@@ -33,8 +33,26 @@ class  ProductoServicio{
 		$sql = "SELECT * FROM  Producto";
 		//$productos[];
 		$x=0;
-		$productosTabla=$conexion->ejecutar($sql)
-		foreach($productoTabla as $productosTabla){
+		$productosTabla=$conexion->ejecutarConsulta($sql);
+		
+		while($productoTabla = mysqli_fetch_object($productosTabla)){
+			$productos[$x]=$this->crearObeto($productoTabla);
+			$x++;
+		}
+		return $productos;
+	
+	}
+	
+	public function findByCategoria($idCategoria) {
+		// abrir conexión BD
+		$conexion = new Conexion();
+		// generar SQL del DAO
+		$sql = "SELECT * FROM  Producto WHERE CategoriaId=".$idCategoria.";";
+		//$productos[];
+		$x=0;
+		$productosTabla=$conexion->ejecutarConsulta($sql);
+		
+		while($productoTabla = mysqli_fetch_object($productosTabla)){
 			$productos[$x]=$this->crearObeto($productoTabla);
 			$x++;
 		}
@@ -68,11 +86,16 @@ class  ProductoServicio{
 	
 	public function crearObeto($resultado){
 		$Producto = new Producto();
+		$Producto->Id=$resultado->Id;
 		$Producto->CodigoBarras=$resultado->CodigoBarras;
+		$Producto->Imagen=$resultado->Imagen;
 		$Producto->Nombre=$resultado->Nombre;
 		$Producto->Descripcion=$resultado->Descripcion;
 		$Producto->PrecioEntrada=$resultado->PrecioEntrada;
-
+		$Producto->PrecioSalida=$resultado->PrecioSalida;
+		$Producto->Categoria=$resultado->CategoriaId;
+		$Producto->InventarioMinimo=$resultado->InventarioMin;
+		$Producto->Activo=$resultado->EsActivo;
 		//$ProductoServicio = new ProductoServicio();
 		//$Producto->Producto=$ProductoServicio->findByID($resultado->idProducto);
 		return $Producto; 
