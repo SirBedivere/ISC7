ff1d410a5efa625ffb6501e38147e1fada3bd6c8
CarlosArceo <master_carlos_1@hotmail.com>
2015-11-23 20:40:41 -0600

    Cambios
    Cambios

--- Design/EncabezadoAdmin.php
+++ Design/EncabezadoAdmin.php
@@ -2,16 +2,12 @@
 <nav class="navbar navbar-inverse">
 	<div class="container-fluid">
 	<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="container-fluid">
+	<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
-				<nav class="navbar navbar-default">
-					<div class="container-fluid">
-						<div class="navbar-header">
-							  <a class="navbar-brand" href="http://localhost/jorge/index.php">
-								<img class="Logo" src="../Css/logo.jpg" width="100%" height="100%" >
-							  </a>
-						</div>
-					</div>
-				</nav>
+		  <a class="navbar-header" href="http://localhost/ISC7/index.php">
+			<img class="Logo" src="../Css/logo.jpg" width="100%" height="100%" >
+		  </a>
 		</div>
 	<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -23,37 +19,22 @@
 						<input type="text" class="form-control" placeholder="Search">
 					</div>
 						<button type="submit" class="btn btn-default">Buscar</button>
+						
 				</form>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Usuario<span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Eliminar Usuario</a></li>
-							<li><a href="#">Editar Usuario</a></li>
-							<li><a href="#">Ver Usuario</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">Separated link</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">One more separated link</a></li>
-						</ul>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Producto<span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="http://localhost/jorge/views/altaProducto.php">Agregar Producto</a></li>
-							<li><a href="#">Eliminar Producto</a></li>
-							<li><a href="#">Editar Producto</a></li>
-							<li><a href="http://localhost/jorge/views/catalogoProductos.php">Ver Productos</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">Separated link</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">One more separated link</a></li>
-						</ul>
-				</li>
+				<form class="navbar-form navbar-left" action="../Views/altaProducto.php">
+					<button type="submit" class="btn btn-default">Usuario</button>
+						
+				</form>
+				<form class="navbar-form navbar-left" action="../Views/catalogoProductosAdmin.php">
+					<button type="submit" class="btn btn-default">Producto</button>
+						
+				</form>
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categoria<span class="caret"></span></a>
 					<ul class="dropdown-menu">
-						<li><a href="#">Agregar Categoria</a></li>
+						<li><a href="http://localhost/ISC7/Views/altaCategoria.php">Agregar Categoria</a></li>
 						<li><a href="#">Eliminar Categoria</a></li>
 						<li><a href="#">Editar Categoria</a></li>
 						<li><a href="#">Ver Categoria</a></li>
---
+++ Design/NavegadorAdmin.php
@@ -0,0 +1,27 @@
+<html>
+	<title>Tienda en linea	</title>
+		
+		<head>
+			<meta charset=utf-8>
+			<script src="../Css/jquery.js">
+			</script>
+			<link rel="stylesheet" href="../Css/bootstrap/css/bootstrap.min.css"/>
+			<link rel="stylesheet" href="../Css/bootstrap/css/bootstrap-theme.min.css"/>
+			<script src="../Css/bootstrap/js/bootstrap.min.js">
+			</script>
+			<link rel="stylesheet" type="text/css" href="../Css/main.css"/>		
+			
+		</head>
+		
+			<body>
+				<div class="Encabezado">
+					<?php include "EncabezadoAdmin.php";?>
+				</div>
+				
+				<div class="Contenido">
+				
+				</div>
+				
+		
+				
+			
